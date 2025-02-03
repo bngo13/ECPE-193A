f4c568e7b25ae4b6977fda4d189db297ed89b254
bngo13 <b_ngo13@u.pacific.edu>
2025-02-03 12:52:58 -0800

    Pt2

--- Assignments/HW1/harris.py
+++ Assignments/HW1/harris.py
@@ -54,44 +54,76 @@ def convolve(image, kernel):
 
   return convolution_img
 
-def horizontal_gradient(image, sigma):
+def vertical_gaussian(image, sigma):
   vertical_kernel = GaussianKernel(sigma)
-  temp_horizontal = convolve(image, vertical_kernel)
+  vertical_blur = convolve(image, vertical_kernel)
 
   horizontal_kernel = GaussianDerivative(sigma)
   flipped_horizontal_kernel = np.transpose(horizontal_kernel)
-  horizontal = convolve(temp_horizontal, flipped_horizontal_kernel)
-  return horizontal
+  horizontal_gradient = convolve(vertical_blur, flipped_horizontal_kernel)
+  return (vertical_blur, horizontal_gradient)
 
-def vertical_gradient(image, sigma):
+def horizontal_gaussian(image, sigma):
   vertical_kernel = GaussianKernel(sigma)
-  flipped_vertical_kernel = np.transpose(vertical_kernel)
-  temp_vertical = convolve(image, flipped_vertical_kernel)
+  horizontal_kernel = np.transpose(vertical_kernel)
+  horizontal_blur = convolve(image, horizontal_kernel)
 
   horizontal_kernel = GaussianDerivative(sigma)
-  vertical = convolve(temp_vertical, horizontal_kernel)
-  return vertical
-  pass
+  vertical_gradient = convolve(horizontal_blur, horizontal_kernel)
+  return (horizontal_blur, vertical_gradient)
 
-def covariance(image, window):
-  # Get image shape (height, width)
-  img_shape = image.shape
-  image_height = img_shape[0]
-  image_width = img_shape[1]
-  for i in range(0, image_height):
-    for j in range(0, image_width):
-      x_squared = 0
-      y_squared = 0
-      xy = 0
+def covariance(image, vert_grad, horiz_grad, window):
+  (height, width) = image.shape
+  Z = []
+  for i in range(0, height):
+    for j in range(0, width):
+      ixx = 0
+      iyy = 0
+      ixy = 0
 
       w = window // 2
 
-      for offset_i in range(-w, w):
-        for offset_j in range(-w, w):
-          if (i + offset_i >= 0 and j + offset_j >= 0 and i + offset_i < image_height and j + offset_j < image_width):
-            pass
+      for offseti in range(-w, w + 1):
+        for offsetj in range(-w, w + 1):
+          pixel_offset_i = i + offseti
+          pixel_offset_j = j + offsetj
+          if (pixel_offset_i >= 0 and pixel_offset_j >= 0 and pixel_offset_i < height and pixel_offset_j < width):
+            vert = vert_grad[pixel_offset_i][pixel_offset_j]
+            horiz = horiz_grad[pixel_offset_i][pixel_offset_j]
 
+            ixx += vert.astype(np.int64) * vert.astype(np.int64)
+            iyy += horiz.astype(np.int64) * horiz.astype(np.int64)
+            ixy += vert.astype(np.int64) * horiz.astype(np.int64)
+      
+      Z.append(
+        [
+          [ixx, ixy], 
+          [ixy, iyy]
+        ]
+      )
+  
+  return Z
 
+def find_corners(image, cov_mat, cornerness_val = 0.04):
+  (height, width) = image.shape
+  evalue, evect = np.linalg.eig(cov_mat)
+
+  features = np.zeros((height, width))
+
+  i = 0
+  j = 0
+  for eval in evalue:
+    if j >= width:
+      i += 1
+      j = 0
+    
+    val = (eval[0] * eval[1]) - (cornerness_val * ((eval[0] + eval[1]) ** 2))
+    features[i][j] = val
+
+    j += 1
+  
+  return features
+  
 
 def main():
   # Get file
@@ -101,11 +133,18 @@ def main():
   # Read the image as grayscale
   image = cv2.imread(filename, 0)
 
-  image = np.array([[8, 24, 48, 32, 16]])
-  kernel = np.array([[1/4, 1/2, 1/4]])
+  (vertical_blur, horiz_grad) = vertical_gaussian(image, 1)
+  (horizontal_blur, vert_grad) = horizontal_gaussian(image, 1)
 
-  new_image = convolve(image, kernel)
+  cov_mat = covariance(image, vert_grad, horiz_grad, 7)
+
+  top_features = find_corners(image, cov_mat)
+
   breakpoint()
+
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
+
 
 
 if __name__ == "__main__":
