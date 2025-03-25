ab96ebb5da4e2a310692263080a282e5ecb0b1a4
bngo13 <b_ngo13@u.pacific.edu>
2025-03-25 10:31:17 -0700

    Update types

--- Assignments/PA3/HC_naive.py
+++ Assignments/PA3/HC_naive.py
@@ -37,7 +37,7 @@ __global__ void convolution(float *image, float *convImg, float *kernel, int ima
     }
 }
 
-__global__ void covariance(float *vert_grad, float *horiz_grad, float32 *cov_mat, int image_height, int image_width, int window) {
+__global__ void covariance(float *vert_grad, float *horiz_grad, float *cov_mat, int image_height, int image_width, int window) {
     // Initialize Vars
     int64_t ixx = 0;
     int64_t iyy = 0;
