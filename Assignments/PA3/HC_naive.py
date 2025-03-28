7035a1b149f76e802cdf829833ce484f65d42cae
bngo13 <b_ngo13@u.pacific.edu>
2025-03-28 11:26:19 -0700

    Remove image writing

--- Assignments/PA3/HC_naive.py
+++ Assignments/PA3/HC_naive.py
@@ -410,12 +410,6 @@ def main():
       for (y,x, _) in top_features:
         f.write(f"{x} {y}\n")
 
-    import cv2
-    img = image.copy()
-    for (y,x, _) in top_features:
-        cv2.putText(img, 'X', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 2)
-    imwrite("corners.pgm", img)
-
     te = time.time()
 
     # Print Format
